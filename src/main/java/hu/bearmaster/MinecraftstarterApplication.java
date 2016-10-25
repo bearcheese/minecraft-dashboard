2c543f30cc19639d00465a89f574dd5141c13a66
bearcheese <bearcheese@gmail.com>
2016-10-25 21:52:33 +0200

    Initial spring boot app added

---
+++ src/main/java/hu/bearmaster/MinecraftstarterApplication.java
@@ -0,0 +1,12 @@
+package hu.bearmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinecraftstarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinecraftstarterApplication.class, args);
+	}
+}
